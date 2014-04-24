4a0d92b496d66900ecb49a9e30e7753b438f44f1
agarofi <devnull@localhost>
2014-04-24 20:03:38 +0300

    interim MAINTENANCE

--- webserverlist/gameServerClass.php
+++ webserverlist/gameServerClass.php
@@ -3,10 +3,25 @@ class gameServerClass
 {
     public $_ip;
     public $_port;
+    public $_lastHeartbeatTS;
     
-    public function __construct($ip, $port)
+    public function __construct($ip, $port, $useDate = FALSE)
     {
         $this->_ip = $ip;
         $this->_port = $port;
+        $this->setHeartbeatTS($useDate);
+    }
+    
+    public function setHeartbeatTS($useDate = FALSE)
+    {
+        if ($useDate === FALSE)
+            $this->_lastHeartbeatTS = date("U");
+        else
+            $this->_lastHeartbeatTS = $useDate;
+    }
+    
+    public function getServers()
+    {
+    
     }
 }
--- webserverlist/getservers.php
+++ webserverlist/getservers.php
@@ -1,9 +1,13 @@
 <?php
-@require_once "gameServerClass.php";
+
+require_once "gameServerClass.php";
+require_once "inc/serversdb.php";
 
 $listOfServers = Array();
 
 // access the DB
+$db = new serversDB();
+$listOfServers = $db->getServers();
 
 $listOfServers[] = new gameServerClass('127.0.0.1', 23);
 $listOfServers[] = new gameServerClass('127.0.0.1', 24);
@@ -13,3 +17,4 @@ foreach ($listOfServers as $server)
     echo ('  '.$server->_ip.'  '.$server->_port . '
 ');
 }
+
