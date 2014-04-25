1892aeb8d743a8383eee1524e90852c3b8631305
agarofi <devnull@localhost>
2014-04-25 09:56:04 +0300

    use memcache as persistence MAINTENANCE

--- webserverlist/inc/serversdb.php
+++ webserverlist/inc/serversdb.php
@@ -2,18 +2,5 @@
 
 class serversDB
 {
-    private function open($mode)
-    {
-        return fopen("db/database.txt", $mode);
-    }
-    
-    private function openRead()
-    {
-        return $this->open("r");
-    }
-
-    private function openWrite()
-    {
-        return $this->open("w");
-    }
+    // use memcache!!  
 }
