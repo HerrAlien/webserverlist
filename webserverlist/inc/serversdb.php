e1ea338867e33d66b11c25a87d6b428acc4aae00
Herr_Alien <devnull@localhost>
2014-04-27 07:45:26 +0300

    memcached server, remove game server class

--- webserverlist/inc/serversdb.php
+++ webserverlist/inc/serversdb.php
@@ -1,19 +1,53 @@
 <?php
 
+// memcached based
 class serversDB
 {
-    private function open($mode)
+  private $m_timestampList;
+  private $m_heartbeatTimeout;
+  
+  private function __construct()
+  {
+    $this->m_timestampList = new array();
+    $this->m_heartbeatTimeout = 1800; // in seconds, 30 minutes
+  }
+  
+  public function tickServer($ip, $port)
+  {
+    $key = $ip.":".$port;
+    $this->m_timestampList[key] = date("U");
+    $this->cleanDeadServers()
+  }
+  
+  // unset to delete
+  public function getServers()
+  {
+    $this->cleanDeadServers()
+    return array_keys($this->m_timestampList);
+  }
+  
+  private function cleanDeadServers()
+  {
+      $currentTime = date("U");
+      $deadTime = $currentTime - $this->m_heartbeatTimeout;
+      $len = count ($this->m_timestampList);
+      for ($i = $len - 1; $i >=0; $i--)
+      {
+          if($deadTime >= $this->m_timestampList[$i])
+            unset ($this->m_timestampList[$i]);
+      }
+  }
+  
+  public static function getInstance()
+  {
+    $cache = new Memcached ("webseverlist-4F7ECB99-9216-40BA-BD3F-6879742D92C0");
+    $instance = $cache->get("serversDB");
+    if ($instance === FALSE)
     {
-        return fopen("db/database.txt", $mode);
+      $instance = new serversDB();
+      $cache->set("serversDB", $instance);
     }
-    
-    private function openRead()
-    {
-        return $this->open("r");
-    }
-
-    private function openWrite()
-    {
-        return $this->open("w");
-    }
+    return $instance;
+  }
+  
 }
