870dc166d0f161dbade470580251c8007b7b7bf6
agarofi <devnull@localhost>
2014-04-30 18:51:10 +0300

    memcached based ... MAINTENANCE

--- webserverlist/getservers.php
+++ webserverlist/getservers.php
@@ -1,20 +1,15 @@
 <?php
 
-require_once "gameServerClass.php";
 require_once "inc/serversdb.php";
 
-$listOfServers = Array();
-
-// access the DB
-$db = new serversDB();
-$listOfServers = $db->getServers();
-
-$listOfServers[] = new gameServerClass('127.0.0.1', 23);
-$listOfServers[] = new gameServerClass('127.0.0.1', 24);
-
-foreach ($listOfServers as $server)
-{
-    echo ('  '.$server->_ip.'  '.$server->_port . '
-');
-}
-
+    $game = $_REQUEST['game'];
+    
+    $db = serversDB::getInstance($game);
+    $servers = $db->getServers();
+    echo count(array_keys($servers));
+    foreach (array_keys($servers) as $server)
+    {
+        echo $server.'
+';
+    }
+    
